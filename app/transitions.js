ab85f49e080fadb3a3baed6ab4e4cc03c7da4f5f
Sam Selikoff <sam.selikoff@gmail.com>
2019-01-14 19:15:45 -0500

    Episode 8

--- app/transitions.js
+++ app/transitions.js
@@ -1,9 +1,6 @@
 export default function(){
-  // Add your transitions here, like:
-  //   this.transition(
-  //     this.fromRoute('people.index'),
-  //     this.toRoute('people.detail'),
-  //     this.use('toLeft'),
-  //     this.reverse('toRight')
-  //   );
+  this.transition(
+    this.hasClass('fading-menu'),
+    this.use('crossFade', { duration: 100 })
+  );
 }
